2a4293c599c4bb7c8efff2fb2669e3dc1729be22
Ruslan <matveev.ruslan@gmail.com>
2019-02-06 11:07:30 +0300

    enable / disable using sms, mockup checkIntrusion procedure

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,42 +1,72 @@
 #include "SIM800.h"
 
-boolean isEnabled = false;
+boolean isSecurityEnabled = false;
 char* controlMSISDN[] = {"+79260617034", "+79190148644"}; 
-    
+
+boolean isAllowedMSISDN(String msisdn) {
+	uint8_t size = sizeof(controlMSISDN) / sizeof(controlMSISDN[0]);
+	for (uint8_t c = 0; c < size; c++) {
+		if (msisdn.equals(controlMSISDN[c])) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void enableSecurity(String responseMSISDN) {
+	if (isSecurityEnabled) {
+		SIM800::sendSMS(responseMSISDN, "охрана уже включена");
+		return;
+	}
+	isSecurityEnabled = true;
+	SIM800::sendSMS(responseMSISDN, "постановка на охрану");
+}
+
+void disableSecurity(String responseMSISDN) {
+	if (!isSecurityEnabled) {
+		SIM800::sendSMS(responseMSISDN, "охрана уже отключена");
+		return;
+	}
+	isSecurityEnabled = false;
+	SIM800::sendSMS(responseMSISDN, "снятие с охраны");
+}
+
+void checkIntrusion() {
+	if (!isSecurityEnabled) return;
+	if (digitalRead(7) === HIGH) {
+		// do something
+	}
+}
 
 void setup() {
 	Serial.begin(9600);
 	SIM800::init(9600, 12, 14);
 }
 
-boolean isAllowedMSISDN(String msisdn) {
-  uint8_t size = sizeof(controlMSISDN) / sizeof(controlMSISDN[0]);
-  for (uint8_t c = 0; c < size; c++) {
-    if (msisdn.equals(controlMSISDN[c])) {
-      return true;
-    }
-  }
-  return false;
-}
-
-
 void loop() {
+
 	switch (SIM800::update()) {
 
 		case SIM800::CALL:
-      if (!isAllowedMSISDN(SIM800::msisdn)) break;
-  		if (isEnabled = !isEnabled) {
-				SIM800::sendSMS(SIM800::msisdn, "включено");
+			if (!isAllowedMSISDN(SIM800::msisdn)) break;
+			if (isSecurityEnabled) {
+				disableSecurity(SIM800::msisdn);
 			} else {
-				SIM800::sendSMS(SIM800::msisdn, "выключено");
+				enableSecurity(SIM800::msisdn);
 			}
 			break;
 
 		case SIM800::SMS:
-      if (!isAllowedMSISDN(SIM800::msisdn)) break;
-      Serial.print("SMS = ");
-      Serial.println(SIM800::text);
+			if (!isAllowedMSISDN(SIM800::msisdn)) break;
+			if (SIM800::text.equals("1")) {
+				enableSecurity(SIM800::msisdn);
+			} else if (SIM800::text.equals("0")) {
+				disableSecurity(SIM800::msisdn);
+			}
 			break;
 
 	}
+
+	checkIntrusion();
+
 }
